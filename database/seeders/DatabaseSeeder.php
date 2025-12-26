75f57717d4327f45a67c2fe59a77a0e93fe09fca
mavrenkov <vasiliymavrenkov@gmail.com>
2025-12-26 14:23:32 +0300

    chore: add fake data for development

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
+use App\Models\Document;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Visitor;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Department::factory(15)->create();
+
+
+        Visitor::factory(10)
+            ->has(Document::factory()->passport())
+            ->create();
+
+        Visitor::factory(10)
+            ->has(Document::factory()->license())
+            ->create();
+
+        Visitor::factory(10)
+            ->has(Document::factory()->other())
+            ->create();
     }
 }
